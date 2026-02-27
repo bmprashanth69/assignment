538864fdebeb350464a2f7ef0812a38085404c1d
bmprashanth69 <bmprashanth30@gmail.com>
2026-02-27 17:00:23 +0530

    Add files via upload

---
+++ Module3/Dog1.java
@@ -0,0 +1,27 @@
+package Assign3;
+class Animal1
+{
+	void sound()
+	{
+   System.out.println("Animal makes a sound");
+	}
+	void sound(String name)
+	{
+   System.out.println("Animal name is: " + name);
+	}
+}
+public class Dog1 extends Animal1
+{
+	void sound()
+	{
+   System.out.println("Dog barks");
+	}
+	public static void main(String[] args)
+	{
+		Animal1 a = new Animal1();
+	    a.sound();
+	    a.sound("Tommy");
+	    Animal1 d = new Dog1();
+	    d.sound();
+	}
+}
---
+++ Module3/Polymorphism.java
@@ -0,0 +1,35 @@
+package Assign3;
+class Shape
+{
+  void draw()
+  {
+      System.out.println("Drawing a shape");
+  }
+}
+class Circle extends Shape 
+{
+  @Override
+  void draw()
+  {
+      System.out.println("Drawing a circle");
+  }
+}
+class Rectangle extends Shape 
+{
+  @Override
+  void draw()
+  {
+      System.out.println("Drawing a rectangle");
+  }
+}
+public class Polymorphism
+{
+  public static void main(String[] args)
+  {
+      Shape s;
+      s = new Circle();
+      s.draw();
+      s = new Rectangle();
+      s.draw();
+  }
+}
